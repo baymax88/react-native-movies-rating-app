5bbb18284d06db2036c1f913742d5e9c8be92418
baymax88 <smartbay1@outlook.com>
2020-05-12 02:54:12 +0800

    added Home screen component

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
 import Home from './src/scenes/Home'
+import Movie from './src/scenes/Movie'
 import Review from './src/scenes/Review'
 import SearchResult from './src/scenes/SearchResult'
 import LogIn from './src/scenes/LogIn'
@@ -21,6 +22,7 @@ export default function App() {
         headerMode="none"
       >
         <Stack.Screen name="Home" component={Home}></Stack.Screen>
+        <Stack.Screen name="Movie" component={Movie}></Stack.Screen>
         <Stack.Screen name="Review" component={Review}></Stack.Screen>
         <Stack.Screen name="SearchResult" component={SearchResult}></Stack.Screen>
         <Stack.Screen name="LogIn" component={LogIn}></Stack.Screen>
--- src/scenes/Movie.js
+++ src/scenes/Movie.js
@@ -6,6 +6,7 @@ export default function Movie() {
     return (
         <View>
             <Header loggedIn={false} />
+
         </View>
     )
 }
